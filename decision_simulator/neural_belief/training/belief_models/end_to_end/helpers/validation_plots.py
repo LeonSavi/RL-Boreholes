a9abdea94a88b5b56f77592a1f071bf5036063be
Dennis <dennis.vriend1@hotmail.com>
2026-06-06 13:35:43 +0200

    Added a null-test for the end-to-end encoders

--- decision_simulator/neural_belief/training/belief_models/end_to_end/helpers/validation_plots.py
+++ decision_simulator/neural_belief/training/belief_models/end_to_end/helpers/validation_plots.py
@@ -268,7 +268,7 @@ def save_sequential_val_plots(
             ax.set_xticks([])
             ax.set_yticks([])
 
-        fig.colorbar(im_pred, ax=[axes[:, 1], axes[:, 2]], shrink=0.6, label="ore value")
+        fig.colorbar(im_pred, ax=list(axes[:, 1]) + list(axes[:, 2]), shrink=0.6, label="ore value")
         if has_unc and im_unc is not None:
             fig.colorbar(im_unc, ax=axes[:, 3], shrink=0.6, label="uncertainty")
             fig.colorbar(im_err, ax=axes[:, 4], shrink=0.6, label="abs error")
